65df36536a5b4191f45fcf6ce7c34aa81c369f8f
ChaimaBSlima <chaimabslima@outlook.fr>
2024-04-25 13:30:52 +0100

    clean repositry

--- python_variable_annotations/0-add.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-''' 
-chaima ben slima 
-'''
-
-def add(a: float, b: float) -> float:
-    ''' Return sum of a and b. '''
-    return a + b
--- python_variable_annotations/3-to_str.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-''' 
-chaima ben slima 
-'''
-
-def to_str(n: float) -> str:
-    ''' Return n as a string. '''
-    return str(n)
